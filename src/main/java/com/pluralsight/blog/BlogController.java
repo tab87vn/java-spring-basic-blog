e9c4b6c975e1a84cda4185924707b69247febad3
Tuan-Anh BUI <tuananh.hufs@gmail.com>
2022-06-26 16:04:05 +0200

    Module 2

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,31 @@
 package com.pluralsight.blog;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+
 @Controller
 public class BlogController {
+    
+    private PostRepository postRepository;
+    
+    public BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+    
     @RequestMapping("/")
     public String listPosts(ModelMap data) {
-        data.put("title", "Blog Post 1");
+        // data.put("title", "Blog Post 1");
 
+        List<Post> allPosts = this.postRepository.getAllPosts();
+
+        data.put("posts", allPosts);
+    
         return "home";
     }
 }
