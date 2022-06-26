51303b3410309007e2addc702cbbf7918db47868
Tuan-Anh BUI <tuananh.hufs@gmail.com>
2022-06-26 16:57:01 +0200

    Module 4

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pluralsight.blog.data.PostRepository;
@@ -28,4 +29,13 @@ public class BlogController {
     
         return "home";
     }
+    
+    @RequestMapping("/posts/{id}")
+    public String postDetails(@PathVariable Long id, ModelMap modelMap) {
+        Post post = postRepository.findById(id);
+
+        modelMap.put("post", post);
+
+        return "post-details"; 
+    }    
 }
