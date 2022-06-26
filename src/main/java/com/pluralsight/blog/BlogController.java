e39c006adf877a56890ea38ee3a3da2122258d8c
Tuan-Anh BUI <tuananh.hufs@gmail.com>
2022-06-26 15:46:29 +0200

    Module 1

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,8 +1,18 @@
 package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BlogController {
+    @RequestMapping("/")
+    public String listPosts(ModelMap data) {
+        data.put("Title", "Blog post 1");
+
+        return "home";
+    }
+
+
 
 }
